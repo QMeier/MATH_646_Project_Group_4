a65c679b0bdca10b762afdecbdd6af789dfc3b66
ThomasGartman <thomasgartman@ku.edu>
2018-11-17 09:21:29 -0600

    Starting the construction of our plot generator in MATLAB

---
+++ PlotGenerator3D.m
@@ -0,0 +1,14 @@
+%PlotGenerator3D in MATH_646_Project_Group_4
+%Version 0.0.0 Last Edited November 17th, 2018
+%
+%This function takes in information on a 2D Schrodinger equation, creates a
+%.stl file, and returns a 3D plot of the waveform.
+%
+%
+%
+%
+%
+%
+function [] = PlotGenerator3D()
+    disp('Test 1 Online');
+end
